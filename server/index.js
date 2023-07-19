25a79e818b2f024cee028854e8550fb7fa3f3313
Dev-Radthpon.JS <radthapon747628@gmail.com>
2023-07-19 22:28:12 +0700

    fix function login and cors

--- server/index.js
+++ server/index.js
@@ -9,14 +9,24 @@ import postRoutes from "./routes/posts.js"
 import commentRoutes from "./routes/comments.js"
 import likeRoutes from "./routes/likes.js"
 import cookieParser from 'cookie-parser';
-
-// Useing app
 const app = express()
-const port = 3000
+
 // middleware
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
 app.use(express.json())
 app.use(cors())
 app.use(cookieParser())
+
+// Useing app
+
+const port = 3000
+
 
 
 // Routes Useing
